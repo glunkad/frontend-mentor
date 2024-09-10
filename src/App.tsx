02b8088db9c1c7bbf44499c548b0e8c24af7fd30
Gunjan <gunjan.lunkad@medimaze.in>
2024-09-10 09:34:00 +0530

    feat: second challenge css updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import {RecipePage} from "./components/recipe-page-main";
 import {SocialLinksProfile} from "./components/social-links-profile-main";
+import {FaqAccordion} from "./components/faq-accordion-main";
 
 
 function App() {
   return (
       // <RecipePage />
-      <SocialLinksProfile />
+      // <SocialLinksProfile />
+      <FaqAccordion />
   )
 }
 
---
+++ src/components/faq-accordion-main/FaqAccordion.tsx
@@ -0,0 +1,33 @@
+import "./style.scss"
+export const FaqAccordion = () => {
+    return (
+        <>
+            <div className="wrapper1">
+
+
+                <div className="card">
+                    <div className="card-header">
+                        <div className="icon"></div>
+                        <h1>FAQs</h1>
+                    </div>
+                    <div className="card-body">
+                        <div className="card-body-title">
+                            <div className="title-text">What is Frontend Mentor, and how will it help me?</div>
+                            <div className="icon"></div>
+                        </div>
+                        <div className="card-body-text">
+                            <p>
+                                Frontend Mentor offers realistic coding challenges to help developers improve their
+                                frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
+                                all levels and ideal for portfolio building.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="wrapper2">
+            </div>
+        </>
+    );
+}
